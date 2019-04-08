e4d98f6ccf5d0e00de3d3f40c17785c22acc917c
moewah <46947401+moewah@users.noreply.github.com>
2019-04-08 13:46:55 +0800

    添加自定义图标

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
         <link href="favicon.ico" rel="shortcut icon">
         <script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
         <link rel="stylesheet" href="css/serverchan.min.css" type="text/css" />
+        <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/tip.min.css" type="text/css" />
+        
         <!-- 自定义背景 -->
         <style>
             body:before{
@@ -31,22 +33,22 @@
     <body>
         <div id="form-bg" style="">
             <form role="form" action="sc.php" method="post" id="send_form" class="well">
-                <h4 style="text-align: center;margin-top: 0 !important;">发送消息</h4>
+                <h4 style="text-align: center;margin-top: 0 !important"><i class="fa fa-weixin"></i> 发送消息</h4>
                 <div class="form-group">
-                    <label for="text">消息标题</label>
+                    <label for="text"><i class="fa fa-info-circle"></i> 消息标题</label>
                     <input type="text" class="form-control" id="text" name="text" placeholder="必填，最长256字节" onkeydown="if(event.keyCode==13) return false;" value="" required>
                 </div>
                 <div class="form-group">
-                    <label for="tel">联系手机</label>
+                    <label for="tel"><i class="fa fa-phone"></i> 联系手机</label>
                     <input type="tel" class="form-control" id="tel" name="tel" placeholder="必填，填写您的11位手机号" onkeydown="if(event.keyCode==13) return false;" value="" required>
-                </div>                
-                <div class="form-group">
-                    <label for="contact">联系邮箱</label>
-                    <input type="email" class="form-control" id="contact" name="contact" placeholder="选填，如果需要我回复请填上邮箱" onkeydown="if(event.keyCode==13) return false;" value="">
                 </div>
                 <div class="form-group">
-                    <label for="desp">消息内容</label>
-                    <textarea class="form-control" id="desp" row="3" placeholder="消息内容，最长64K，选填，支持MarkDown" name="desp"></textarea>
+                    <label for="contact"><i class="fa fa-envelope-o"></i> 您的邮箱</label>
+                    <input type="email" class="form-control" id="contact" name="contact" placeholder="选填，填写你的邮箱，并注意查收邮件" onkeydown="if(event.keyCode==13) return false;" value="">
+                </div>
+                <div class="form-group">
+                    <label for="desp"><i class="fa fa-commenting"></i> 问题描述</label>
+                    <textarea class="form-control" id="desp" row="3" placeholder="请描述一下你的问题，最长64K，选填，支持MarkDown，换行请敲两次回车键" name="desp"></textarea>
                 </div>
                 <button type="submit" id="submit-btn" class="btn btn-primary">发送消息</button>
                 <button type="reset" class="btn btn-primary" onclick="$('input').val('');$('textarea').html('')">　重置　</button>
@@ -54,4 +56,5 @@
         </div>
         <script src="js/submit.min.js"></script>
     </body>
+
 </html>
